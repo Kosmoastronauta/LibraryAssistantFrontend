bc71f91d0a38fb85df915ff1a35ed42f30b55745
Mateusz Gil <mateusz_gil@outlook.com>
2019-08-13 10:44:38 +0200

    Added not found component and added routes for all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,33 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BooksComponent } from './books/books.component';
+import {Router, RouterModule, Routes} from "@angular/router";
+import { NotFoundComponent } from './not-found/not-found.component';
 
+const appRoutes: Routes = [
+  {
+    path: 'books',
+    component: BooksComponent
+  },
+  {
+    path: '',
+    component: BooksComponent,
+    pathMatch: 'full'
+  },
+  {
+    path:'**',
+    component:NotFoundComponent
+  }
+];
 @NgModule({
   declarations: [
     AppComponent,
-    BooksComponent
+    BooksComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     AppRoutingModule
   ],
   providers: [],
